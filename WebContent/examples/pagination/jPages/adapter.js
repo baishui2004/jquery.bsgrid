b2c0329bfe9f8767b0897a4e100aa0496bcd2827
bs2004 <bs2004@163.com>
2014-09-01 16:10:31 +0800

    新增：bsgrid with jPages示例。

---
+++ WebContent/examples/pagination/jPages/adapter.js
@@ -0,0 +1,94 @@
+/**
+ * jPages adapter for bsgrid.
+ *
+ * jQuery.bsgrid v1.20 by @Baishui2004
+ * Copyright 2014 Apache v2 License
+ * https://github.com/baishui2004/jquery.bsgrid
+ */
+/**
+ * require common.js, grid.js.
+ *
+ * @author Baishui2004
+ * @Date September 1, 2014
+ */
+$.fn.bsgrid.getCurPage = function (options) {
+    return options.curPage;
+};
+
+$.fn.bsgrid.refreshPage = function (options) {
+    $('#' + options.pagingId).data('bsgrid-curPage', '');
+    $.fn.bsgrid.getGridObj(options.gridId).page($.fn.bsgrid.getCurPage(options));
+};
+
+$.fn.bsgrid.firstPage = function (options) {
+    $.fn.bsgrid.getGridObj(options.gridId).page(1);
+};
+
+$.fn.bsgrid.prevPage = function (options) {
+    var curPage = $.fn.bsgrid.getCurPage(options);
+    if (curPage <= 1) {
+        alert($.bsgridLanguage.isFirstPage);
+        return;
+    }
+    $.fn.bsgrid.getGridObj(options.gridId).page(curPage - 1);
+};
+
+$.fn.bsgrid.nextPage = function (options) {
+    var curPage = $.fn.bsgrid.getCurPage(options);
+    if (curPage >= options.totalPages) {
+        alert($.bsgridLanguage.isLastPage);
+        return;
+    }
+    $.fn.bsgrid.getGridObj(options.gridId).page(curPage + 1);
+};
+
+$.fn.bsgrid.lastPage = function (options) {
+    $.fn.bsgrid.getGridObj(options.gridId).page(options.totalPages);
+};
+
+$.fn.bsgrid.gotoPage = function (options, goPage) {
+    if (goPage == undefined) {
+        return;
+    }
+    if (isNaN(goPage)) {
+        alert($.bsgridLanguage.needInteger);
+    } else if (parseInt(goPage) < 1 || parseInt(goPage) > options.totalPages) {
+        alert($.bsgridLanguage.needRange(1, options.totalPages));
+    } else {
+        $.fn.bsgrid.getGridObj(options.gridId).page(goPage);
+    }
+};
+
+$.fn.bsgrid.initPaging = function (options) {
+    $('#' + options.pagingOutTabId).after('<ul id="' + options.pagingId + '-ul" style="width: 0; height: 0; overflow: hidden;"></ul>');
+    $('#' + options.pagingOutTabId + ' td').append('<div id="' + options.pagingId + '" class="holder" style="margin-top: 0px;"></div>');
+};
+
+$.fn.bsgrid.setPagingValues = function (options) {
+    var ulInnerHtml = new StringBuilder();
+    for (var i = 0; i < options.totalRows; i++) {
+        ulInnerHtml.append('<li></li>');
+    }
+    $('#' + options.pagingId + '-ul').html(ulInnerHtml.toString());
+    $('#' + options.pagingId).jPages({
+        containerID: options.pagingId + '-ul',
+        perPage: options.settings.pageSize,
+        startPage: options.curPage,
+        first: $.bsgridLanguage.pagingToolbar.firstPage, // false
+        previous: $.bsgridLanguage.pagingToolbar.prevPage, // "← previous"
+        next: $.bsgridLanguage.pagingToolbar.nextPage, // "next →"
+        last: $.bsgridLanguage.pagingToolbar.lastPage, // false
+        midRange: 6,
+        startRange: 2,
+        endRange: 2,
+        callback: function (pages, items) { // function(pages, items) { }
+            var curPage_o = $.trim($('#' + options.pagingId).data('bsgrid-curPage'));
+            if (curPage_o == '') {
+                $('#' + options.pagingId).data('bsgrid-curPage', 1);
+            } else if (curPage_o != '' && parseInt(curPage_o) != pages.current) {
+                $('#' + options.pagingId).data('bsgrid-curPage', pages.current);
+                $.fn.bsgrid.getGridObj(options.gridId).page(pages.current);
+            }
+        }
+    });
+};
